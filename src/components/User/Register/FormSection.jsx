ffcd0bfb415a7652c63fc8600c6048db9f0defa3
mukesh <mukeshmehta2041@gmail.com>
2023-07-16 14:33:33 +0530

    Adding Fprm validation by zod and get the Form data

--- src/components/User/Register/FormSection.jsx
+++ src/components/User/Register/FormSection.jsx
@@ -1,45 +1,69 @@
-const FormSection = () => {
+import PropTypes from "prop-types";
+
+const FormSection = (props) => {
+  const { handleSubmit, register, errors, reset, onSubmit } = props;
   return (
     <div className="w-full lg:w-1/2 px-4">
       <div className="px-6 lg:px-12 py-12 lg:py-24 bg-white rounded-lg shadow-lg">
-        <form>
+        <form
+          onSubmit={handleSubmit((data) => {
+            onSubmit(data);
+            reset();
+          })}
+        >
           <h3 className="mb-10 text-2xl text-gray-800 font-bold">
             Register Account
           </h3>
           <div className="mb-6">
             <input
+              {...register("firstName")}
               id="firstName"
               className="w-full px-4 py-3 placeholder-gray-400 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="text"
               placeholder="First Name"
             />
+            <div className="text-red-400 mb-2">
+              {errors.firstName && <p> {errors.firstName.message}</p>}
+            </div>
           </div>
           <div className="mb-6">
             <input
+              {...register("lastName")}
               id="lastName"
               className="w-full px-4 py-3 placeholder-gray-400 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="text"
               placeholder="Last Name"
             />
+            <div className="text-red-400 mb-2">
+              {errors.lastName && <p> {errors.lastName.message}</p>}
+            </div>
           </div>
           <div className="mb-6">
             <input
+              {...register("email")}
               id="email"
               className="w-full px-4 py-3 placeholder-gray-400 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="email"
               placeholder="example@gmail.com"
             />
+            <div className="text-red-400 mb-2">
+              {errors.email && <p> {errors.email.message}</p>}
+            </div>
           </div>
           <div className="mb-6">
             <input
+              {...register("password")}
               id="password"
               className="w-full px-4 py-3 placeholder-gray-400 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="password"
               placeholder="Password"
             />
+            <div className="text-red-400 mb-2">
+              {errors.password && <p> {errors.password.message}</p>}
+            </div>
           </div>
           <button
-            className="w-full px-6 py-4 bg-blue-500 hover:bg-blue-600 text-white rounded-full transition duration-200"
+            className="w-full px-6 py-4 bg-blue-500 hover:bg-blue-600 text-white rounded-full font-semibold transition duration-200"
             type="submit"
           >
             Register
@@ -48,6 +72,14 @@ const FormSection = () => {
       </div>
     </div>
   );
+};
+
+FormSection.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+  reset: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 
 export default FormSection;
--- src/components/User/Register/Register.jsx
+++ src/components/User/Register/Register.jsx
@@ -1,14 +1,44 @@
 import FormSection from "./FormSection";
 import HeadingSection from "./HeadingSection";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const RegisterSchema = z.object({
+  firstName: z.string().min(3, "First Name is required").max(50),
+  lastName: z.string().min(3, "Last Name is Required").max(50),
+  email: z.string().email("Please enter a valid email address"),
+  password: z.string().min(8, "Password is Required").max(50),
+});
 
 const Register = () => {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(RegisterSchema),
+  });
+
+  // submit the form
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <section className="relative py-20 2xl:py-40 bg-gradient-to-r from-blue-900 to-blue-600 overflow-hidden">
       <div className="container px-4 mx-auto">
         <div className="max-w-5xl mx-auto">
           <div className="flex flex-wrap items-center -mx-4">
             <HeadingSection />
-            <FormSection />
+            <FormSection
+              handleSubmit={handleSubmit}
+              register={register}
+              reset={reset}
+              errors={errors}
+              onSubmit={onSubmit}
+            />
           </div>
         </div>
       </div>
