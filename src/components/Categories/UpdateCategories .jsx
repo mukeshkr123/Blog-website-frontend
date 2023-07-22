d8b598f2064af16db8e9fae92bc5fd7d95603562
mukesh <mukeshmehta2041@gmail.com>
2023-07-22 23:09:11 +0530

    update dispatch and delete dispatch

--- src/components/Categories/UpdateCategories .jsx
+++ src/components/Categories/UpdateCategories .jsx
@@ -1,57 +1,63 @@
-import { BiBookOpen } from "react-icons/bi";
-import { AiOutlinePlus } from "react-icons/ai";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { FaPlusCircle, FaBookOpen } from "react-icons/fa";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
-import { createCategoryAction } from "../../redux/slices/category/categorySlices";
 
-const categorySchema = z.object({
-  title: z.string().min(5, "At least 5 characters required"),
-});
+import {
+  deleteCategoryAction,
+  fetchCategoryAction,
+  updateCategoryAction,
+} from "../../redux/slices/category/categorySlices";
+import { useParams } from "react-router-dom";
 
 const UpdateCategory = () => {
+  const { id } = useParams();
   const dispatch = useDispatch();
+
+  // Fetch single category
+  useEffect(() => {
+    dispatch(fetchCategoryAction(id));
+  }, [dispatch, id]);
+
+  // Get data from store
+  const state = useSelector((state) => state?.category);
+  const { loading, appErr, serverErr, category } = state;
+
+  // react-hook-form
   const {
-    register,
     handleSubmit,
+    register,
     formState: { errors },
   } = useForm({
-    resolver: zodResolver(categorySchema),
     defaultValues: {
-      title: "",
+      title: category?.title || "",
     },
   });
 
-  const onSubmit = (data) => {
-    dispatch(createCategoryAction(data));
+  // Form submit handler
+  const onSubmit = (values) => {
+    // Dispatch the action for updating the category
+    dispatch(updateCategoryAction({ title: values.title, id }));
   };
-
-  //get the store
-  const state = useSelector((state) => state.category);
-
-  const { appErr, serverErr, loading } = state;
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <BiBookOpen className="mx-auto h-12 w-auto" />
+          <FaBookOpen className="mx-auto h-12 w-auto" />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Add New Category
+            Update Category
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600 font-medium">
-            These are the categories users will select when creating a post
+          <p className="mt-2 text-center text-sm text-gray-600">
+            These are the categories user will select when creating a post
           </p>
           {/* Display error */}
-          <div className="text-red-500 text-center text-lg">
-            {appErr ||
-              (serverErr && (
-                <p>
-                  {serverErr}
-                  {appErr}
-                </p>
-              ))}
+          <div>
+            {appErr || serverErr ? (
+              <h2 className="text-red-500 text-center text-lg">
+                {serverErr} {appErr}
+              </h2>
+            ) : null}
           </div>
         </div>
         {/* Form */}
@@ -59,23 +65,19 @@ const UpdateCategory = () => {
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
-              <label htmlFor="new-category" className="sr-only">
-                Name
+              <label htmlFor="title" className="sr-only">
+                Title
               </label>
               {/* Title */}
               <input
-                {...register("title")}
+                {...register("title", { required: "Title is required" })}
                 type="text"
-                id="new-category"
-                name="title"
                 autoComplete="text"
-                required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-center focus:z-10 sm:text-sm"
                 placeholder="New Category"
               />
               <div className="text-red-400 mb-2">
-                {/* Display form validation errors here */}
-                {errors.title && <p> {errors?.title?.message}</p>}
+                {errors.title && errors.title.message}
               </div>
             </div>
           </div>
@@ -83,23 +85,42 @@ const UpdateCategory = () => {
           <div>
             <div>
               {/* Submit */}
-              <button
-                type="submit"
-                disabled={loading}
-                className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white ${
-                  loading ? "bg-gray-600" : "bg-indigo-600 hover:bg-indigo-700"
-                } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <AiOutlinePlus
-                    className={`h-5 w-5 text-yellow-500 ${
-                      loading ? "group-hover:text-indigo-400" : ""
-                    }`}
-                    aria-hidden="true"
-                  />
-                </span>
-                {loading ? "Adding...." : "Add new Category"}
-              </button>
+              {loading ? (
+                <button
+                  disabled
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 "
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <FaPlusCircle
+                      className="h-5 w-5 text-yellow-500 group-hover:text-indigo-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                  Loading please wait...
+                </button>
+              ) : (
+                <>
+                  <button
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                      <FaPlusCircle
+                        className="h-5 w-5 text-yellow-500 group-hover:text-indigo-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    Update Category
+                  </button>
+                  <button
+                    onClick={() => dispatch(deleteCategoryAction(id))}
+                    type="button"
+                    className="group mt-2 relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Delete Category
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </form>
