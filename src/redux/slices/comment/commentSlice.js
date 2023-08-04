884dcdd671fd77deaac73e651226f7da4980387d
mukesh <mukeshmehta2041@gmail.com>
2023-08-04 20:32:44 +0530

    bug fix for comment slices

---
+++ src/redux/slices/comment/commentSlice.js
@@ -0,0 +1,58 @@
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const baseUrl = "http://localhost:5000";
+
+// create a comment
+export const createCommentAction = createAsyncThunk(
+  "category/create",
+  async (comment, { rejectWithValue, getState }) => {
+    try {
+      const userAuth = getState()?.users?.userAuth;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userAuth?.token}`,
+        },
+      };
+
+      const { data } = await axios.post(
+        `${baseUrl}/api/comments`,
+        { description: comment?.title, postId: comment?.postId },
+        config
+      );
+      return data;
+    } catch (error) {
+      if (!error.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//slices
+const commentSlice = createSlice({
+  name: "comment",
+  initialState: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createCommentAction.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createCommentAction.fulfilled, (state, action) => {
+        state.loading = false;
+        state.commentCreated = action?.payload;
+      })
+      .addCase(createCommentAction.rejected, (state, action) => {
+        state.commentCreated = undefined;
+        state.loading = false;
+        if (action.payload) {
+          state.appErr = action.payload.message;
+        } else {
+          state.serverErr = "Something went wrong. Please try again later.";
+        }
+      });
+  },
+});
+
+export default commentSlice.reducer;
--- src/redux/store/store.jsx
+++ src/redux/store/store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slices/user/userSlices";
 import categoryReducer from "../slices/category/categorySlices";
 import post from "../slices/posts/postSlices";
+import comment from "../slices/comment/commentSlice";
 
 const store = configureStore({
   reducer: {
     users: userReducer,
     category: categoryReducer,
     post,
+    comment,
   },
 });
 
