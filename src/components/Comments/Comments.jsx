f542342876c98bf9f23e889eda943beaadc6dc35
mukesh <mukeshmehta2041@gmail.com>
2023-08-04 20:19:10 +0530

    adding comment bug fix

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,20 +1,27 @@
-import { z } from "zod";
+import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+//Form schema
+const formSchema = z.object({
+  description: z.string().nonempty("Description is required"),
+});
 
 const AddComment = ({ postId }) => {
-  const Schema = z.object({
-    description: z.string().min(5, "Please enter a comment"),
-  });
-
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors },
-  } = useForm({ resolver: zodResolver(Schema) });
+  } = useForm({
+    resolver: zodResolver(formSchema),
+  });
 
   const onSubmit = (data) => {
-    console.log(data);
+    const newData = {
+      postId,
+      description: data.description,
+    };
+    console.log(newData);
   };
 
   return (
@@ -23,17 +30,20 @@ const AddComment = ({ postId }) => {
         onSubmit={handleSubmit(onSubmit)}
         className="mt-1 flex max-w-sm m-auto"
       >
-        <input
-          {...register("description")}
-          type="text"
-          name="text"
-          id="text"
-          className="shadow-sm focus:ring-indigo-500  mr-2 focus:border-indigo-500 block w-full p-2 border-1 sm:text-sm border-gray-300 rounded-md"
-          placeholder="Add New comment"
+        <Controller
+          name="description"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <input
+              {...field}
+              type="text"
+              className="shadow-sm focus:ring-indigo-500  mr-2 focus:border-indigo-500 block w-full p-2 border-1 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Add New comment"
+            />
+          )}
         />
-        {errors?.description && (
-          <div className="text-red-400 mt-1">{errors.description.message}</div>
-        )}
+
         <button
           type="submit"
           className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -41,6 +51,9 @@ const AddComment = ({ postId }) => {
           Submit
         </button>
       </form>
+      <div className="text-red-400 mb-2 mt-2">
+        {errors?.description?.message}
+      </div>
     </div>
   );
 };
